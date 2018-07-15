9e7ee366e047bc95a5c57ec1fa8c2a9083aa596d
Zilbar, Gidi(gzilbar) <gzilbar@paypal.com>
2018-07-15 23:02:31 +0300

    added a different sampling method

--- GD_logistic_reg.py
+++ GD_logistic_reg.py
@@ -13,6 +13,16 @@ def gen_sample(instances, features):
     y = np.where(y_val > 0, 1, 0)
     return X, y, true_weights
 
+def gen_sample_2(instances, features):
+    X1 = np.random.multivariate_normal(5*np.ones(features), np.eye(features), size=instances) + 1.15
+    X2 = np.random.multivariate_normal(3*np.ones(features), np.eye(features), size=instances)
+    bias = np.ones(shape=[2 * instances, 1])
+    y1 = np.ones([instances, 1])
+    y2 = np.zeros([instances, 1])
+    X = np.vstack((X1, X2))
+    X = np.column_stack((bias, X))
+    y = np.vstack((y1, y2))
+    return X, y
 def gen_intial_weights(features):
     """Generate initial weights"""
     w = np.random.normal(loc=0.0, scale=0.5, size=[features + 1, 1])
@@ -44,37 +54,72 @@ def calc_error_rate(sigmoid_vec, y):
     error_rate = np.sum(errors) / len(y)
     return error_rate
 
+def plot_classifier(w):
+    intercept = - w[0] / w[2]
+    slope = - w[1] / w[2]
+    line = lambda x: intercept + slope * x
+    x = np.arange(0, 8, 0.2)
+    line = np.vectorize(line)
+    y = line(x)
+    plt.plot(x, y)
+
+
 """Now lets run our experiment"""
 instances = 15000
 features = 100
 gamma = 0.0001
-min_error_rate = 0.004
+min_error_rate = 0.015
 counter = 0
 error_rate_vec = []
 
-X, y, true_weights = gen_sample(instances, features)
+# X, y, true_weights = gen_sample(instances, features)
+# w = gen_intial_weights(features)
+# sigmoid_vec = calc_sigmoid_vec(X, w)
+# error_rate = calc_error_rate(sigmoid_vec, y)
+# while error_rate > min_error_rate:
+#     sigmoid_vec = calc_sigmoid_vec(X, w)
+#     gd_step = calc_gradient_step(X, y, w, gamma, sigmoid_vec)
+#     w = make_gradient_step(w, gd_step)
+#     error_rate = calc_error_rate(sigmoid_vec, y)
+#     error_rate_vec.append(error_rate)
+#     #print(error_rate)
+#     counter += 1
+# print(counter)
+# print('True Weights:', true_weights)
+# print('Final Weights:', w)
+# plt.plot(error_rate_vec)
+# plt.show()
+
+# TODO: generate sample X, y using two clusters where linear shattering of the data is not possible
+instances = 200
+features = 2
+X, y = gen_sample_2(instances, features)
+x1, y1 = X[0:instances, 1], X[0:instances, 2]
+x2, y2 = X[instances::, 1], X[instances::, 2]
+
+# plt.show()
+
+#Second Experiment using the second method of sampling:
+X,  y  = gen_sample_2(instances, features)
 w = gen_intial_weights(features)
 sigmoid_vec = calc_sigmoid_vec(X, w)
 error_rate = calc_error_rate(sigmoid_vec, y)
+error_rate_vec = []
 while error_rate > min_error_rate:
     sigmoid_vec = calc_sigmoid_vec(X, w)
     gd_step = calc_gradient_step(X, y, w, gamma, sigmoid_vec)
     w = make_gradient_step(w, gd_step)
     error_rate = calc_error_rate(sigmoid_vec, y)
     error_rate_vec.append(error_rate)
-    #print(error_rate)
-    counter += 1
-print(counter)
-print('True Weights:', true_weights)
-print('Final Weights:', w)
+
 plt.plot(error_rate_vec)
 plt.show()
 
-# TODO: generate sample X, y using two clusters where linear shattering of the data is not possible
 
-
-
-
+plt.scatter(x1, y1, color='blue')
+plt.scatter(x2, y2, color='red')
+plot_classifier(w)
+plt.show()
 
 
 
